0c8501cbca997877c8b5370f46cd5fbd858f26d8
森田陸斗 <mrmrt1121@gmail.com>
2022-03-02 23:43:11 +0900

    Viewの実装.実際にはmainに記述

--- lib/main.dart
+++ lib/main.dart
@@ -1,72 +1,107 @@
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mvvm_app/controller/api_request.dart';
+import 'package:mvvm_app/model/state.dart';
+import 'package:mvvm_app/model/user.dart';
 
 void main() async {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'MVC practice',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        //VisualDensity により視覚的な詰まり具合(密度)を調整
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+class MyHomePage extends ConsumerWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() async {
-    await fetchUsers();
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final users = ref.watch(userStateProvider);
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        leading: const Icon(Icons.menu),
+        title: const Text('Random User Info'),
+        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.search))],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: users.when(
+          data: (user) {
+            return RefreshIndicator(
+                child: ListView.builder(
+                  itemCount: user.length,
+                  itemBuilder: (context, index) {
+                    var fullName = user[index].name.title +
+                    " " +
+                    user[index].name.first +
+                    " " +
+                    user[index].name.last;
+                    return Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: ListTile(
+                            title: Row(
+                              children: [
+                                CircleAvatar(
+                                  backgroundImage:
+                                    NetworkImage(user[index].picture.large),
+                                  radius: 24.0,
+                                ),
+                                const SizedBox(width: 20.0),
+                                Expanded(
+                                    child: Column(
+                                      crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          fullName.toString(),
+                                          style: const TextStyle(fontSize: 17.0),
+                                          overflow: TextOverflow.ellipsis,
+                                        ),
+                                        const SizedBox(height: 10.0),
+                                        Text(
+                                          user[index].email.toString(),
+                                          style: const TextStyle(
+                                            fontSize: 16.0,
+                                            color: Colors.grey,
+                                            overflow: TextOverflow.ellipsis,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    );
+                  },
+                ),
+                onRefresh: () async {
+                  ref.refresh(userStateProvider);
+                },
+            );
+          },
+          error: (error, stackTrace) =>
+              Center(child: Text(error.toString())),
+          loading: () => const Center(child: CircularProgressIndicator()),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
--- lib/model/state.dart
+++ lib/model/state.dart
@@ -2,6 +2,6 @@ import 'package:mvvm_app/model/user.dart';
 import 'package:mvvm_app/controller/api_request.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final counterProvider = FutureProvider<List<User>>((ref) async {
+final userStateProvider = FutureProvider<List<User>>((ref) async {
   return fetchUsers();
 });
