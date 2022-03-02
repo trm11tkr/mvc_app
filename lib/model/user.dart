71f19be0aad9e4157922a619bfb67ccbfa4cc08d
森田陸斗 <mrmrt1121@gmail.com>
2022-03-02 15:54:05 +0900

    モデルの作成

---
+++ lib/model/user.dart
@@ -0,0 +1,64 @@
+// import 'package:json_annotation/json_annotation.dart';
+// import 'package:json_serializable/json_serializable.dart';
+// import 'package:build_runner/build_runner.dart';
+
+class User {
+  User({required this.gender,required this.email, required this.name});
+
+  late String gender;
+  late String email;
+  late Name name;
+  late Picture picture;
+
+  User.fromJson(Map json) {
+    gender = json['gender'];
+    email = json['email'];
+    name = (json['name'] != null ? Name.fromJson(json['name']) : null)!;
+    picture = (json['picture'] != null ? Picture.fromJson(json['picture']) : null)!;
+
+  }
+}
+
+class Name {
+  Name({required this.title, required this.first, required this.last});
+
+  late String title;
+  late String first;
+  late String last;
+
+  Name.fromJson(Map json) {
+    title = json['title'];
+    first = json['first'];
+    last = json['last'];
+  }
+
+  Map toJson() {
+    final Map data = {};
+    data['title'] = this.title;
+    data['first'] = this.first;
+    data['last'] = this.last;
+    return data;
+  }
+}
+
+class Picture {
+  Picture({required this.large, required this.medium, required this.thumbnail});
+
+  late String large;
+  late String medium;
+  late String thumbnail;
+
+  Picture.fromJson(Map json) {
+    large = json['title'];
+    medium = json['first'];
+    thumbnail = json['last'];
+  }
+
+  Map toJson() {
+    final Map data = {};
+    data['title'] = this.large;
+    data['first'] = this.medium;
+    data['last'] = this.thumbnail;
+    return data;
+  }
+}
